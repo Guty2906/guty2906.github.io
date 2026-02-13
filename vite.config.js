b349a32574a88dc5f223365e11f5a84663b910b4
Juan Mejia <juanmejia234255@gmail.com>
2026-02-13 14:04:39 -0500

    Cambvios

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/guty2906.github.io/', // 👈 CAMBIA ESTO por el nombre exacto de tu repositorio
+  base: '/', // 👈 CAMBIA ESTO por el nombre exacto de tu repositorio
 })
